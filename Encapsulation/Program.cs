ca08adc60c860823fac42345e94123ae366898df
theerachai1234 <133219123+theerachai1234@users.noreply.github.com>
2025-04-16 14:43:40 +0700

    main

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Encapsulation.Class;
+
+EncapsulationStudent student = new EncapsulationStudent();
+student.SetName("Chopper");
+student.SetStudentID(1138);
+student.SetAge(22);
+student.ShowInfo();
